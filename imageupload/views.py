5cd0fbd804a2219ccc29774b0456d2a95ba382ec
Shashwat Jain <shashwatjain@Shashwats-MBP.lan>
2018-05-14 23:34:19 +0800

    Image upload Working

--- imageupload/views.py
+++ imageupload/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import pdb;
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
 class imageuploadview(TemplateView):
     def get(self, request, **kwargs):
+        # pdb.set_trace()
         return render(request, 'imageUI.html', context=None)
