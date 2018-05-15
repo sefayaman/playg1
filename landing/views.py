38f1c603f1582bc4c774ce195ee7e1390eb784c4
EC2 Default User <ec2-user@ip-172-31-19-19.ap-southeast-1.compute.internal>
2018-05-15 08:12:55 +0000

    Added landing page1

--- landing/views.py
+++ landing/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-class landingview():
+class landingview(TemplateView):
     def get(self, request, **kwargs):
         # pdb.set_trace()
         return render(request, 'index.html', context=None)
