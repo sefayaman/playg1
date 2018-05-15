3c07bd08504df00515ec57ab55ed67cdd25d66d0
EC2 Default User <ec2-user@ip-172-31-19-19.ap-southeast-1.compute.internal>
2018-05-15 03:10:55 +0000

    Resolving intalled apps issue

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloworld',
-    'imageupload'
+    'helloworld.apps.HelloworldConfig',
+    'imageupload.apps.ImageuploadConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
